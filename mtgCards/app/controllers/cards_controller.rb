5317615aa59765076f7b7556f05c20011ee1c91d
alcesalces11331 <rollin.metzger@gmail.com>
2019-11-16 16:47:34 -0600

    Fill out cards methods

--- mtgCards/app/controllers/cards_controller.rb
+++ mtgCards/app/controllers/cards_controller.rb
@@ -1,15 +1,27 @@
 class CardsController < ApplicationController
 
     def index
+        @cards = Card.all
+        render json: @cards
     end
 
     def show
+        @card = Card.find(params[:id])
+        render json: @card
     end
 
     def create
+        @card = Card.create(
+            name: params["name"]
+            multiverseid: params["id"]
+        )
+        render json: @card
     end
 
     def delete_all
+        Card.delete_all
+        @card = Card.new
+        render json: @card
     end
 
     private
