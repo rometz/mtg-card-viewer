7518affcaba053791408a29752fab10b007b2191
alcesalces11331 <rollin.metzger@gmail.com>
2019-11-17 17:39:28 -0600

    Switch responsibility to appRouter for viewing

---
+++ mtgCards/client/src/router/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function AppRouter() {
+    return (
+        <Router>
+            <div className="App App-body">
+
+            </div>
+        </Router>
+    )
+}
+
+export default AppRouter;
