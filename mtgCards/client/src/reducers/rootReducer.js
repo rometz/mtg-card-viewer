0760656f081b58985e2976b535290d9c1ee91119
alcesalces11331 <rollin.metzger@gmail.com>
2019-11-17 20:25:04 -0600

    Build rootReducer for redux store

---
+++ mtgCards/client/src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import draftingReducer from './draftingReducer';
+import deckReducer from './deckReducer';
+import cardReducer from './cardReducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    drafts: draftingReducer,
+    decks: deckReducer,
+    cards: cardReducer
+});
+
+export default rootReducer;
