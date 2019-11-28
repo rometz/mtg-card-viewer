1a6347c364a6f4f35123d98e7f57e063e613b0a2
alcesalces11331 <rollin.metzger@gmail.com>
2019-11-28 16:54:58 -0600

    Stub out fetchBooster() action

--- mtgCards/client/src/actions/mtgActions/DraftActions.js
+++ mtgCards/client/src/actions/mtgActions/DraftActions.js
@@ -1,7 +1,13 @@
-export function fetchBooster(set) {
+export function fetchBooster() {
+    // needs testing! 11/28/19
     return async (dispatch) => {
         dispatch({type: "LOADING_BOOSTER"});
-        /* create function in CardsController to reflect a route*/
-        /* create a mtg::api controller? */
-    }
+        const url = '/mtgapi/fetchbooster';
+        const response = await fetch(url);
+        const set = await response.json();
+        const data = await set["cards"];
+        return dispatch({
+            type: 'FETCH_CARDS', payload: data
+        });
+    };
 }
