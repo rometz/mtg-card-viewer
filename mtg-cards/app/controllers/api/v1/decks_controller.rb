d896d28dabc8113626e4970e92cd092dfd6a8851
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-05 17:57:27 -0600

    Refactor DecksController for comments/dryness

--- mtg-cards/app/controllers/api/v1/decks_controller.rb
+++ mtg-cards/app/controllers/api/v1/decks_controller.rb
@@ -1,29 +1,55 @@
 module Api::V1
     class DecksController < ApplicationController
+        before_action :set_deck, only: [:show, :update, :destroy]
 
+        # GET /decks
         def index
-            @decks = Deck.all
+            @decks = Deck.order(:id)
             render json: @decks
         end
 
+        # GET /decks/1
         def show
-            @deck = Deck.find(params[:id])
             render json: @deck
         end
 
+        # POST /decks
         def create
-            @deck = Deck.create(
-                name: params["name"]
-            )
-            render json: @deck
+            @deck = Deck.create(deck_params)
+            if @deck.save
+                render json: @deck, status: :created
+            else
+                render json: @deck.errors, status: :unprocessable_entity
+            end
         end
 
-        def delete
-            Deck.delete(params[:id])
+        # PATCH/PUT /decks/1
+        def update
+            if @deck.update(deck_params)
+                render json: @deck
+            else
+                render json: @deck.errors, status: :unprocessable_entity
+            end
+        end
+
+        #DELETE /deck
+        def destroy
+            @deck.destroy
+            if @deck.destroy
+                head :no_content, status: :ok
+            else
+                render json: @deck.errors, status: :unprocessable_entity
+            end
         end
 
         private
 
+        #Callback for commonalities
+        def deck_list
+            @deck = Deck.find(params[:id])
+        end
+
+        #SOP white list
         def deck_params
             params.require(:deck).permit(:name, :id)
         end
