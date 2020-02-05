683982fd6a8591648bd7020167f6270c9300fa89
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-05 17:49:47 -0600

    Refactor CardsController for DRYness & comments

--- mtg-cards/app/controllers/api/v1/cards_controller.rb
+++ mtg-cards/app/controllers/api/v1/cards_controller.rb
@@ -1,21 +1,39 @@
 module Api::V1
     class CardsController < ApplicationController
+        before_action :set_card, only: [:show, :update, :destroy]
 
+        # GET /cards
         def index
-            @cards = Card.all
+            @cards = Card.order(:multiverseid)
             render json: @cards
         end
 
+        # GET /cards/1
         def show
-            @card = Card.find(params[:id])
             render json: @card
         end
 
+        # POST /cards
         def create
             @card = Card.create(card_params)
-            render json: @card
+            if @card.save
+                render json: @card, status: :created
+            else
+                render json: @card.errors, status: :unprocessable_entity
+            end
         end
 
+        # DELETE /cards/1
+        def destroy
+            @card.destroy
+            if @card.destroy
+                head :no_content, status: :ok
+            else
+                render json: @card.errors, status: :unprocessable_entity
+            end
+        end
+
+        #DELETE /cards
         def delete_all
             Card.delete_all
             @card = Card.new
@@ -24,6 +42,12 @@ module Api::V1
 
         private
 
+        # Callback for common setup
+        def card_list
+            @card = Card.find(params[:id])
+        end
+
+        # SOP white list
         def card_params
             params.require(:card).permit(:name, :multiverseid, :url, :deck_id)
         end
