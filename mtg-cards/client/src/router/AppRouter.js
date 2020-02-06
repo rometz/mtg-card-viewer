5c06920e0719299cdc0f584631a9d1356729240a
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-05 20:02:27 -0600

    Add header links to AppRouter

--- mtg-cards/client/src/router/AppRouter.js
+++ mtg-cards/client/src/router/AppRouter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CardContainer from '../containers/CardContainer';
+import Header from '../components/Header';
 
 function AppRouter() {
     return (
         <Router>
             <h1>this is rollin</h1>
             <div className="App App-body">
+                <Header />
                 <Switch>
                     <Route path='/cards' exact component={CardContainer} />
                 </Switch>
