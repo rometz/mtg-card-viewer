4a741cb5c6c1751a6345f6264397aedec537f8a9
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-09 16:56:23 -0600

    Write action fetchAllSets

--- mtg-cards/client/src/actions/DraftActions.js
+++ mtg-cards/client/src/actions/DraftActions.js
@@ -1,5 +1,7 @@
+// load a booster from a set
 export function fetchBooster() {
-    // needs testing! 11/28/19
+    // works as of 02/08
+    // could be further trimmed, but not necessary atm
     return async (dispatch) => {
         dispatch({type: "LOADING_BOOSTER"});
         const url = 'http://localhost:3001/api/v1/mtgapi/fetchbooster';
@@ -8,6 +10,19 @@ export function fetchBooster() {
         const data = await set["cards"];
         return dispatch({
             type: 'FETCH_CARDS', payload: data
+        });
+    };
+}
+
+// load a list of all sets
+export function fetchAllSets() {
+    return async (dispatch) => {
+        dispatch({type: "LOADING_SETS"});
+        const url = 'http://localhost:3001/api/v1/mtgapi/fetchallsets';
+        const response = await fetch(url);
+        const data = await response.json();
+        return dispatch({
+            type: 'FETCH_ALL_SETS', payload: data
         });
     };
 }
