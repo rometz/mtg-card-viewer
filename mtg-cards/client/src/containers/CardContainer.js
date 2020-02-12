3c3039cf80d8c034e53cb7c2713556022bb73618
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-11 18:46:53 -0600

    Update CardContainer for async api call(s).
    This commit initializes useEffect upon mounting for the sole purpose
    of being able to reference the fetched data later on. Furthermore,
    this commit flushes out useSelector for dynamic state referencing, adds
    a variable for future card handling, and a testable button.

--- mtg-cards/client/src/containers/CardContainer.js
+++ mtg-cards/client/src/containers/CardContainer.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import Card from '../components/Card';
 import { useDispatch, useSelector } from 'react-redux';
 //import { createSelector } from 'reselect';
 //import { createCard } from '../actions/CardActions';
 import { fetchBooster } from '../actions/DraftActions';
+import { fetchAllSets } from '../actions/DraftActions';
 
 const CardContainer = () => {
     const dispatch = useDispatch();
-    const cards = useSelector(
-        state => state.cards.createCard,
-        state => state.drafts.fetchBooster
-    )
+    const { cards, booster, sets } = useSelector(state => ({
+        cards: state.cards.createCard,
+        booster: state.drafts.fetchBooster,
+        sets: state.drafts.fetchAllSets
+    }));
+    const cardsReturned = useState([]);
+    
+    // upon mounting, fetch all sets from mtg_sdk
+    useEffect(() => {
+        dispatch(fetchAllSets())
+    }, [])
 
     return (
         <main>
             <button onClick={() => dispatch(fetchBooster())}>Fetch a booster</button>
+            <button onClick={() => sets}>Fetch all Sets</button>
         </main>
     );
 };
