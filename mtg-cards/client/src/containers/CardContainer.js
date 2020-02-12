d44c8f65716606528627b2923eb1b659bb459b00
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-11 20:14:27 -0600

    Create handler function for pure component usage

--- mtg-cards/client/src/containers/CardContainer.js
+++ mtg-cards/client/src/containers/CardContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 //import Card from '../components/Card';
+import SetHandler from '../components/SetHandler';
 import { useDispatch, useSelector } from 'react-redux';
 //import { createSelector } from 'reselect';
 //import { createCard } from '../actions/CardActions';
@@ -14,16 +15,32 @@ const CardContainer = () => {
         sets: state.drafts.fetchAllSets
     }));
     const cardsReturned = useState([]);
+    const showSets = useState(false);
     
     // upon mounting, fetch all sets from mtg_sdk
     useEffect(() => {
         dispatch(fetchAllSets())
-    }, [])
+    }, []);
+
+    handleSetViewer = () => {
+        const setProps = [];
+        sets.forEach(
+            s => setProps.push(s.code)
+        );
+        console.log(setProps);
+        return setProps.map(
+            s => <SetHandler key={s.indexOf(s.code)} namej={s.code} />
+        )
+    }
 
     return (
         <main>
             <button onClick={() => dispatch(fetchBooster())}>Fetch a booster</button>
-            <button onClick={() => sets}>Fetch all Sets</button>
+            <button>Sets</button>
+            
+            <div className="sets-menu">
+                {this.handleSetViewer()}
+            </div>
         </main>
     );
 };
