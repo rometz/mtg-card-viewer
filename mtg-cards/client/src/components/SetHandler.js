d69b74202f4aaf99506a0894815256923bf7a0f9
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-11 20:41:19 -0600

    Consolidate SetHandler li return

--- mtg-cards/client/src/components/SetHandler.js
+++ mtg-cards/client/src/components/SetHandler.js
@@ -4,8 +4,7 @@ const SetHandler = props => {
 
     return(
         <ul>
-            <li id="set-code">{this.props.code}</li>
-            <li id="set-name">{this.props.name}</li>
+            <li id={this.props.code}>{this.props.name}</li>
         </ul>
     );
 };
