335d28b5eb6ce0b0629a733e0f535feab38790f2
alcesalces11331 <rollin.metzger@gmail.com>
2020-02-19 19:46:57 -0600

    Hard code SetsDropDown json for build out

--- mtg-cards/client/src/components/SetsDropDown.js
+++ mtg-cards/client/src/components/SetsDropDown.js
@@ -4,10 +4,20 @@ function SetsDropDown(set) {
     const setItem = useState('');
 
     //initially, we'll hardcode this for something to work with
-
+    const setItemHC = useState([
+        {
+            code: 1,
+            name: "bob"
+        },
+        { code: 2, name: 'bob2'}
+    ])
     return(
         <select>
-            <option value={setItem.code}>{setItem.name}</option>
+            {setItemHC.map(({ code, name}) => (
+                <option key={code} value={code}>
+                    {name}
+                </option>
+            ))}
         </select>
     );
 }
